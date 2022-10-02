03638c715756e956eee7892ecbc6bbac9e5c7732
softwaretestlab <softwaretestlab@hotmail.com>
2022-10-03 09:32:31 +1100

    new

---
+++ src/test/java/SeleniumLearning/AT01_JavaMainMethod.java
@@ -0,0 +1,11 @@
+//package
+package SeleniumLearning;
+//class
+public class AT01_JavaMainMethod {
+    //java main -->method
+    public static void main(String[] args){
+        //code
+        System.out.println("This is my first Java class");
+        System.out.println("This is SoumyaProject");
+    }
+}
---
+++ src/test/java/SeleniumLearning/AT02_JunitLearning.java
@@ -0,0 +1,17 @@
+package SeleniumLearning;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AT02_JunitLearning {
+
+    @Test
+    public void method01(){
+        System.out.println("This is junit method01");
+    }
+
+    @Test
+    public void method02(){
+        System.out.println("This is junit method02");
+    }
+}
